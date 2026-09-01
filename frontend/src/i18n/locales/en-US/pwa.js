e135b0c4c39559bc0b8c5b1515ea5f2902c39b87
HuiStudy <79929708@qq.com>
2026-09-01 09:24:04 +0800

    Update install prompt messages for Hui Cloud Drive

--- frontend/src/i18n/locales/en-US/pwa.js
+++ frontend/src/i18n/locales/en-US/pwa.js
@@ -34,7 +34,7 @@ export default {
       installable: "Installable",
       installing: "Installing",
       installApp: "Install App",
-      installPrompt: "Add CloudPaste to your home screen for a better experience",
+      installPrompt: "Add Hui Cloud Drive to your home screen for a better experience",
     },
 
     // Service Worker status
@@ -88,7 +88,7 @@ export default {
     // Install prompt
     installPrompt: {
       title: "Install App",
-      message: "Add CloudPaste to your home screen for a better experience",
+      message: "Add Hui Cloud Drive to your home screen for a better experience",
       install: "Install",
       later: "Later",
       dismiss: "Later",
